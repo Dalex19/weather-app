9dfb1a5135a0955c1d8b66e8a8a5cf6db075a07f
Dalex19 <dearalex875@gmail.com>
2023-04-04 00:54:10 -0600

    refactor component home

--- screens/index.tsx
+++ screens/index.tsx
@@ -3,66 +3,66 @@ import { StyleSheet, View, Dimensions } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
 
 import Home from "./Home/index";
-import Slider from "../components/Slider"
+import Slider from "../components/Slider";
 
-function RootApp () {
-    const width = Dimensions.get("window").width;
+type Prop = {
+  navigation: any;
+};
 
-    const data = [
-        {
-          image: require('../assets/bg.jpg'),
-          title: "Supplements",
-        },
-        {
-          image: require('../assets/bg1.jpg'),
-          title: "Health",
-        },
-        {
-          image: require('../assets/bg2.jpg'),
-          title: "Training",
-        },
-      ]
+function RootApp({ navigation }: Prop) {
+  const width = Dimensions.get("window").width;
 
-    return(
-        <View style={styles.container}>
-            <Carousel 
-                loop
-                width={width}
-                autoPlay={true}
-                data={data}
-                scrollAnimationDuration={2500}
-                renderItem={({ item }) => (
-                    <Slider 
-                       imgUrl={item.image}
-                       title={item.title}
-                    />
-                )}
-            />
-            <Home />
-        </View>
-    );
+  const data = [
+    {
+      image: require("../assets/bg.jpg"),
+      title: "Supplements",
+    },
+    {
+      image: require("../assets/bg1.jpg"),
+      title: "Health",
+    },
+    {
+      image: require("../assets/bg2.jpg"),
+      title: "Training",
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Carousel
+        loop
+        width={width}
+        autoPlay={true}
+        data={data}
+        scrollAnimationDuration={2000}
+        renderItem={({ item }) => (
+          <Slider imgUrl={item.image} title={item.title} />
+        )}
+      />
+      <Home navigation={navigation} />
+    </View>
+  );
 }
 
 export default RootApp;
 
 const styles = StyleSheet.create({
-    container: {
-      position: "relative",
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    slide: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    backgroundImage: {
-      flex: 1,
-      width: "100%",
-      resizeMode: 'cover',
-      justifyContent: 'center',
-    },
-  });
-  
+  container: {
+    position: "relative",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  slide: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+});
