228f9f01bdef78b943bfe3768f9c8b3d8493d5e6
Dalex19 <dearalex875@gmail.com>
2023-04-04 00:40:40 -0600

    file for routes

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import RootApp from "./screens";
+import LogIn from "./screens/LogIn";
+import SignUp from "./screens/SignUp"
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator >
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name= "Home" options={{ headerShown: false }} component={RootApp} />
+        <Stack.Screen name= "LogIn" options={{ headerShown: false }} component={LogIn} />
+        <Stack.Screen name= "SignUp" options={{ headerShown: false }} component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
