02ffeff53c6f2a031eb5bf25f6dcaa5a87381923
Dalex19 <dearalex875@gmail.com>
2023-04-04 00:52:47 -0600

    format code

--- components/MyBtn/index.tsx
+++ components/MyBtn/index.tsx
@@ -1,33 +1,41 @@
-import React from "react"
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
 interface MainProp {
-    nameBtn: string,
-    paddingBtn: boolean,
-    textColor?: string
+  nameBtn: string;
+  paddingBtn: boolean;
+  textColor?: string;
+  goTo: () => void;
 }
 
-function Main ({ nameBtn, paddingBtn, textColor } : MainProp) {
-    return(
-        <TouchableOpacity style={[ styles.btn, {backgroundColor: paddingBtn ? "white" : ""} ]}>
-            <Text style={[styles.btnText, {color: textColor? textColor : "black"}]}>{nameBtn}</Text>
-        </TouchableOpacity>
-    )
+function Main({ nameBtn, paddingBtn, textColor, goTo }: MainProp) {
+  return (
+    <TouchableOpacity
+      style={[styles.btn, { backgroundColor: paddingBtn ? "white" : "" }]}
+      onPress={goTo}
+    >
+      <Text
+        style={[styles.btnText, { color: textColor ? textColor : "black" }]}
+      >
+        {nameBtn}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
 export default Main;
 
 const styles = StyleSheet.create({
-    btn: {
-        alignItems: "center",
-        paddingHorizontal: 35,
-        paddingVertical: 5,
-        borderRadius: 100,
-        borderWidth: 1,
-        borderColor: 'white',
-    },
-    btnText: {
-        fontSize: 18,
-        fontWeight: "bold",
-    }
-})
+  btn: {
+    alignItems: "center",
+    paddingHorizontal: 35,
+    paddingVertical: 5,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
