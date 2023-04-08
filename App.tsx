2fe9131d1791c0126371e5b81c43253c16d8cbec
Dalex19 <dearalex875@gmail.com>
2023-04-08 00:15:17 -0600

    added new scren in the router

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import React from "react"
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import RootApp from "./screens";
 import LogIn from "./screens/LogIn";
 import SignUp from "./screens/SignUp"
+import Weather from "./screens/Weather";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name= "Home" options={{ headerShown: false }} component={RootApp} />
-        <Stack.Screen name= "LogIn" options={{ headerShown: false }} component={LogIn} />
-        <Stack.Screen name= "SignUp" options={{ headerShown: false }} component={SignUp} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={RootApp}
+          />
+          <Stack.Screen
+            name="LogIn"
+            options={{ headerShown: false }}
+            component={LogIn}
+          />
+          <Stack.Screen
+            name="SignUp"
+            options={{ headerShown: false }}
+            component={SignUp}
+          />
+          <Stack.Screen
+            name="Weather"
+            options={{ headerShown: false }}
+            component={Weather}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
