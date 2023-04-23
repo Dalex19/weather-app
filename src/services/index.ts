5bb1ac32217f1d06507850c81396ced7cad9dacc
Dalex19 <dearalex875@gmail.com>
2023-04-22 22:07:49 -0600

    refac code

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
 import { City } from "../redux/citiesSlice";
 
+const APIKEY : string = "95cf4a6cc1df99fb470485a959c61a03";
+
 async function getWeather(city: string): Promise<City> {
-  const apiKey = "95cf4a6cc1df99fb470485a959c61a03";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   return {
@@ -15,4 +17,4 @@ async function getWeather(city: string): Promise<City> {
 const names = ["Jinotega", "Leon", "Matagalpa", "Managua"];
 
 
-export   {getWeather, names };
+export   { getWeather, names };
